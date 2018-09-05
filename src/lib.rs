f11420b3fb54bdf0bcfa2be86b00cf11c4d9050b
unknown <inv2004@gmail.com>
2018-09-05 12:54:17 -0400

    bookrecord to split price/size

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,12 @@ pub enum Side {
     Buy, Sell
 }
 
+pub struct BookRecord {
+    pub price: f64,
+    pub size: f64,
+    pub id: Uuid
+}
+
 pub struct OrderBook {
     pub book: Vec<VecDeque<(f64, Uuid)>>,
     bid: usize,
@@ -28,19 +34,19 @@ impl OrderBook {
         }
     }
 
-    pub fn init(&mut self, bids: Vec<(f64, f64, Uuid)>, asks: Vec<(f64, f64, Uuid)>) {
-        bids.into_iter().for_each(|(price, size, id)| self.add(Side::Buy, price, size, id));
-        asks.into_iter().for_each(|(price, size, id)| self.add(Side::Sell, price, size, id));
+    pub fn init(&mut self, bids: Vec<BookRecord>, asks: Vec<BookRecord>) {
+        bids.into_iter().for_each(|rec| self.add(Side::Buy, rec));
+        asks.into_iter().for_each(|rec| self.add(Side::Sell, rec));
     }
 
-    pub fn add(&mut self, side: Side, price: f64, size: f64, id: Uuid) {
-        let p_idx = get_idx(price);
+    pub fn add(&mut self, side: Side, rec: BookRecord) {
+        let p_idx = get_idx(rec.price);
         match side {
             Side::Buy if p_idx > self.bid => self.bid = p_idx,
             Side::Sell if p_idx < self.ask => self.ask = p_idx,
             _ => ()
         }
-        self.book[p_idx].push_back((size, id));
+        self.book[p_idx].push_back((rec.size, rec.id));
     }
 
     pub fn _match(&mut self, price: f64, size: f64, id: Uuid) {
@@ -57,17 +63,22 @@ impl OrderBook {
     pub fn done(&mut self, price: f64, id: Uuid) {
         let p_idx = get_idx(price);
         self.book[p_idx].retain(|&(_, it_id)| it_id != id);
+        self.check_ask_bid(p_idx);
     }
 
     pub fn change(&mut self, price: f64, new_size: f64, id: Uuid) {
         let p_idx = get_idx(price);
-        self.book[p_idx]
-            .iter_mut()
-            .for_each(|(it_size, it_id)| {
-                if *it_id == id {
-                    *it_size = new_size;
-                }
-        })
+        if new_size == 0.0 {
+            self.done(price, id);
+        } else {
+            self.book[p_idx]
+                .iter_mut()
+                .for_each(|(it_size, it_id)| {
+                    if *it_id == id {
+                        *it_size = new_size;
+                    }
+                })
+        }
     }
 
     fn check_ask_bid(&mut self, p_idx: usize) {
@@ -106,22 +117,26 @@ impl fmt::Display for OrderBook {
         let asks = self.fmt_side(self.ask..=self.ask+size);
         let bid = self.bid as f64 / 100.0;
         let ask = self.ask as f64 / 100.0;
-        write!(f, "{} | {:.2}   {:.2} | {}", bids, bid, ask, asks)
+        write!(f, "OB: {} | {:.2}   {:.2} | {}", bids, bid, ask, asks)
     }
 }
 
 #[cfg(test)]
-mod tests {   use super::*;
+mod tests {
+    use super::*;
 
     #[test]
     fn test_display() {
         let mut ob = OrderBook::new();
-        ob.init(vec![(3994.96, 0.3, Uuid::new_v4()), (3995.0, 0.5, Uuid::new_v4())],
-                vec![(4005.0, 0.4, Uuid::new_v4()), (4005.02, 0.2, Uuid::new_v4())]);
+        ob.init(vec![BookRecord{price:3994.96, size:0.3, id:Uuid::new_v4()}
+                     , BookRecord{price:3995.0, size:0.5, id:Uuid::new_v4()}],
+                vec![BookRecord{price:4005.0, size:0.4, id:Uuid::new_v4()}
+                     , BookRecord{price: 4005.02, size: 0.2, id: Uuid::new_v4()}]);
 
-        ob.add(Side::Buy, 3994.96, 0.2, Uuid::new_v4());
+        ob.add(Side::Buy, BookRecord{price:3994.96, size:0.2, id: Uuid::new_v4()});
 
-        println!("{}", ob);
+        let str = format!("{}", ob);
+        assert_eq!(str, "OB: 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.5,0,0,0,0.5 | 3995.00   4005.00 | 0.4,0,0.2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
     }
 
     #[test]
@@ -129,13 +144,31 @@ mod tests {   use super::*;
         let mut ob = OrderBook::new();
         let id1 = Uuid::new_v4();
         let id2 = Uuid::new_v4();
-        ob.init(vec![(3994.96, 0.3, id1), (3995.0, 0.5, id2)],
-                vec![(4005.0, 0.4, Uuid::new_v4()), (4005.02, 0.2, Uuid::new_v4())]);
+        ob.init(vec![BookRecord{price:3994.96, size:0.3, id:id1}
+                     , BookRecord{price:3995.0, size:0.5, id:id2}],
+                vec![BookRecord{price:4005.0, size:0.4, id:Uuid::new_v4()}
+                     , BookRecord{price: 4005.02, size: 0.2, id: Uuid::new_v4()}]);
 
-        ob.add(Side::Buy, 3994.96, 0.2, Uuid::new_v4());
-        println!("{}", ob);
+        ob.add(Side::Buy, BookRecord{price:3994.96, size:0.2, id:Uuid::new_v4()});
         ob._match(3995.0, 0.5, id2);
-        println!("{}", ob);
 
+        let str = format!("{}", ob);
+        assert_eq!(str, "OB: 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.5 | 3994.96   4005.00 | 0.4,0,0.2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
+    }
+
+    #[test]
+    fn test_done() {
+        let mut ob = OrderBook::new();
+        let id1 = Uuid::new_v4();
+        let id2 = Uuid::new_v4();
+        ob.init(vec![BookRecord{price:3994.96, size:0.3, id:id1}
+                     , BookRecord{price:3995.0, size:0.5, id:id2}],
+                vec![BookRecord{price:4005.0, size:0.4, id:Uuid::new_v4()}
+                     , BookRecord{price: 4005.02, size: 0.2, id: Uuid::new_v4()}]);
+
+        ob.done(3994.96, id1);
+
+        let str = format!("{}", ob);
+        assert_eq!(str, "OB: 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.5 | 3995.00   4005.00 | 0.4,0,0.2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
     }
 }
