f0906ad8ea866ab1c03b437c18ba4e8589b240ef
unknown <inv2004@gmail.com>
2018-09-06 01:39:24 -0400

    cargo fmt

--- examples/coinbase.rs
+++ examples/coinbase.rs
@@ -1,18 +1,22 @@
 extern crate coinbase_pro_rs;
-extern crate orderbook_rs;
 extern crate futures;
+extern crate orderbook_rs;
 extern crate tokio;
 extern crate tokio_tungstenite;
-#[macro_use] extern crate failure;
+#[macro_use]
+extern crate failure;
 
-use futures::{Future, Stream};
-use futures::future::Either;
-use coinbase_pro_rs::{WSFeed, WS_URL};
-use coinbase_pro_rs::structs::wsfeed::*;
-use coinbase_pro_rs::{Public, ASync, MAIN_URL};
 use coinbase_pro_rs::structs::public::*;
-use orderbook_rs::{OrderBook, BookRecord};
-use std::sync::{Arc, Mutex, atomic::{AtomicUsize, Ordering}};
+use coinbase_pro_rs::structs::wsfeed::*;
+use coinbase_pro_rs::{ASync, Public, MAIN_URL};
+use coinbase_pro_rs::{WSFeed, WS_URL};
+use futures::future::Either;
+use futures::{Future, Stream};
+use orderbook_rs::{BookRecord, OrderBook};
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    Arc, Mutex,
+};
 
 use coinbase_pro_rs::structs::reqs::OrderSide;
 use orderbook_rs::Side;
@@ -22,56 +26,74 @@ enum Error {
     #[fail(display = "coinbase")]
     Coinbase(#[cause] coinbase_pro_rs::CBError),
     #[fail(display = "websocket")]
-    Websocket(#[cause] coinbase_pro_rs::WSError)
+    Websocket(#[cause] coinbase_pro_rs::WSError),
 }
 
 fn convert_side(side: OrderSide) -> Side {
     match side {
         OrderSide::Buy => Side::Buy,
-        OrderSide::Sell => Side::Sell
+        OrderSide::Sell => Side::Sell,
     }
 }
 
 fn convert_record(rec: &BookRecordL3) -> BookRecord {
-    BookRecord{price: rec.price, size: rec.size, id: rec.order_id}
+    BookRecord {
+        price: rec.price,
+        size: rec.size,
+        id: rec.order_id,
+    }
 }
 
 fn get_seq(full: &Full) -> &usize {
     match full {
-        Full::Open(Open{sequence, ..}) => sequence,
-        Full::Done(Done::Limit{sequence, ..}) => sequence,
-        Full::Done(Done::Market{sequence, ..}) => sequence,
-        Full::Match(Match{sequence, ..}) => sequence,
-        Full::Change(Change{sequence, ..}) => sequence,
-        Full::Received(Received::Limit{sequence, ..}) => sequence,
-        Full::Received(Received::Market{sequence, ..}) => sequence,
-        _ => unimplemented!()
+        Full::Open(Open { sequence, .. }) => sequence,
+        Full::Done(Done::Limit { sequence, .. }) => sequence,
+        Full::Done(Done::Market { sequence, .. }) => sequence,
+        Full::Match(Match { sequence, .. }) => sequence,
+        Full::Change(Change { sequence, .. }) => sequence,
+        Full::Received(Received::Limit { sequence, .. }) => sequence,
+        Full::Received(Received::Market { sequence, .. }) => sequence,
+        _ => unimplemented!(),
     }
 }
 
-fn reload(client: &Public<ASync>) -> impl Future<Item=Book<BookRecordL3>, Error=Error>{
+fn reload(client: &Public<ASync>) -> impl Future<Item = Book<BookRecordL3>, Error = Error> {
     println!("reload");
-    client.get_book("BTC-USD")
-        .map_err(Error::Coinbase)
+    client.get_book("BTC-USD").map_err(Error::Coinbase)
 }
 
 fn process_full(ob: &mut OrderBook, full: Full) {
-//    println!("{:?}", full);
+    //    println!("{:?}", full);
     match full {
-        Full::Received(..)
-            => (),
-        Full::Open(Open{price, remaining_size: size, order_id: id, side, ..})
-            => ob.open(convert_side(side), BookRecord { price, size, id }).unwrap_or(()),
-        Full::Done(Done::Limit {price, order_id: id, ..})
-            => ob.done(price, id).unwrap_or(()),
-        Full::Done(Done::Market {..})
-            => (),
-        Full::Match(Match{size, price, maker_order_id: id, ..})
-            => ob._match(price, size, id).unwrap_or(()),
-        Full::Change(Change{new_size: size, price, order_id: id, ..})
-            => ob.change(price, size, id).unwrap_or(()),
-        _
-            => println!("other: {:?}", full)
+        Full::Received(..) => (),
+        Full::Open(Open {
+            price,
+            remaining_size: size,
+            order_id: id,
+            side,
+            ..
+        }) => ob
+            .open(convert_side(side), BookRecord { price, size, id })
+            .unwrap_or(()),
+        Full::Done(Done::Limit {
+            price,
+            order_id: id,
+            ..
+        }) => ob.done(price, id).unwrap_or(()),
+        Full::Done(Done::Market { .. }) => (),
+        Full::Match(Match {
+            size,
+            price,
+            maker_order_id: id,
+            ..
+        }) => ob._match(price, size, id).unwrap_or(()),
+        Full::Change(Change {
+            new_size: size,
+            price,
+            order_id: id,
+            ..
+        }) => ob.change(price, size, id).unwrap_or(()),
+        _ => println!("other: {:?}", full),
     }
 }
 
@@ -86,41 +108,35 @@ fn main() {
     let f = stream
         .map_err(Error::Websocket)
         .for_each(move |msg| {
-            match msg {
-               Message::Full(full) => {
-                   let new_sequence = get_seq(&full).to_owned();
-                   let old_sequence = sequence.load(Ordering::SeqCst);
-                   if new_sequence > 1 + old_sequence {
-                       let ob2 = ob.clone();
-                       let sequence2 = sequence.clone();
-                       return Either::A(reload(&client)
-                           .and_then(move |book| {
-                               let bids = book.bids.iter()
-                                   .map(convert_record)
-                                   .collect::<Vec<_>>();
-                               let asks = book.asks.iter()
-                                   .map(convert_record)
-                                   .collect::<Vec<_>>();
-                               let mut ob = ob2.lock().unwrap();
-                               ob.reload(bids, asks);
-                               sequence2.store(book.sequence, Ordering::SeqCst);
-                               println!("{}", ob);
-                               Ok(())
-                           }))
-                   } else if new_sequence <= old_sequence {
-                       ;
-                   } else {
-                       sequence.fetch_add(1, Ordering::SeqCst);
-                       let mut ob = ob.lock().unwrap();
-                       process_full(&mut ob, full);
-                       println!("{}", ob);
-                   }
-               },
-               _ => ()
+        match msg {
+            Message::Full(full) => {
+                let new_sequence = get_seq(&full).to_owned();
+                let old_sequence = sequence.load(Ordering::SeqCst);
+                if new_sequence > 1 + old_sequence {
+                    let ob2 = ob.clone();
+                    let sequence2 = sequence.clone();
+                    return Either::A(reload(&client).and_then(move |book| {
+                        let bids = book.bids.iter().map(convert_record).collect::<Vec<_>>();
+                        let asks = book.asks.iter().map(convert_record).collect::<Vec<_>>();
+                        let mut ob = ob2.lock().unwrap();
+                        ob.reload(bids, asks);
+                        sequence2.store(book.sequence, Ordering::SeqCst);
+                        println!("{}", ob);
+                        Ok(())
+                    }));
+                } else if new_sequence <= old_sequence {
+;
+                } else {
+                    sequence.fetch_add(1, Ordering::SeqCst);
+                    let mut ob = ob.lock().unwrap();
+                    process_full(&mut ob, full);
+                    println!("{}", ob);
+                }
             }
-            Either::B(futures::future::result(Ok(())))
-        });
+            _ => (),
+        }
+        Either::B(futures::future::result(Ok(())))
+    });
 
     tokio::run(f.map_err(|e| println!("{:?}", e)));
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,43 @@
 extern crate uuid;
 
-use std::ops::RangeInclusive;
-use std::fmt;
 use std::collections::VecDeque;
+use std::fmt;
+use std::ops::RangeInclusive;
 pub use uuid::Uuid;
 
 #[derive(Debug)]
 pub enum Side {
-    Buy, Sell
+    Buy,
+    Sell,
 }
 
 #[derive(Debug)]
 pub struct BookRecord {
     pub price: f64,
     pub size: f64,
-    pub id: Uuid
+    pub id: Uuid,
 }
 
 pub struct OrderBook {
     pub book: Vec<VecDeque<(f64, Uuid)>>,
     bid: usize,
-    ask: usize
+    ask: usize,
 }
-
 
 impl OrderBook {
     pub fn new() -> Self {
         Self {
-            book: vec![VecDeque::new(); 20000*100],
+            book: vec![VecDeque::new(); 20000 * 100],
             bid: std::usize::MIN,
-            ask: std::usize::MAX
+            ask: std::usize::MAX,
         }
     }
 
     pub fn reload(&mut self, bids: Vec<BookRecord>, asks: Vec<BookRecord>) -> Option<()> {
-        bids.into_iter().try_for_each(|rec| self.open(Side::Buy, rec))?;
-        asks.into_iter().try_for_each(|rec| self.open(Side::Sell, rec))?;
+        bids.into_iter()
+            .try_for_each(|rec| self.open(Side::Buy, rec))?;
+        asks.into_iter()
+            .try_for_each(|rec| self.open(Side::Sell, rec))?;
         Some(())
     }
 
@@ -50,11 +52,11 @@ impl OrderBook {
 
     pub fn open(&mut self, side: Side, rec: BookRecord) -> Option<()> {
         let p_idx = self.get_idx(rec.price)?;
-//        println!("{} {:?} {:?}", p_idx, side, rec);
+        //        println!("{} {:?} {:?}", p_idx, side, rec);
         match side {
             Side::Buy if p_idx > self.bid => self.bid = p_idx,
             Side::Sell if p_idx < self.ask => self.ask = p_idx,
-            _ => ()
+            _ => (),
         }
         self.book[p_idx].push_back((rec.size, rec.id));
         Some(())
@@ -84,13 +86,11 @@ impl OrderBook {
         if new_size == 0.0 {
             self.done(price, id);
         } else {
-            self.book[p_idx]
-                .iter_mut()
-                .for_each(|(it_size, it_id)| {
-                    if *it_id == id {
-                        *it_size = new_size;
-                    }
-                })
+            self.book[p_idx].iter_mut().for_each(|(it_size, it_id)| {
+                if *it_id == id {
+                    *it_size = new_size;
+                }
+            })
         }
         Some(())
     }
@@ -118,8 +118,7 @@ impl OrderBook {
                 let size = orders.iter().map(|x| x.0).sum::<f64>();
                 let _count = orders.len();
                 size.to_string()
-            })
-            .collect::<Vec<_>>()
+            }).collect::<Vec<_>>()
             .join(",")
     }
 }
@@ -127,11 +126,11 @@ impl OrderBook {
 impl fmt::Display for OrderBook {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         if self.bid == std::usize::MIN || self.ask == std::usize::MAX {
-            return write!(f, "OB: empty")
+            return write!(f, "OB: empty");
         }
         let size = 20;
-        let bids = self.fmt_side((self.bid-size)..=self.bid);
-        let asks = self.fmt_side(self.ask..=self.ask+size);
+        let bids = self.fmt_side((self.bid - size)..=self.bid);
+        let asks = self.fmt_side(self.ask..=self.ask + size);
         let bid = self.bid as f64 / 100.0;
         let ask = self.ask as f64 / 100.0;
         write!(f, "OB: {} | {:.2}   {:.2} | {}", bids, bid, ask, asks)
@@ -145,12 +144,41 @@ mod tests {
     #[test]
     fn test_display() {
         let mut ob = OrderBook::new();
-        ob.reload(vec![BookRecord{price:3994.96, size:0.3, id:Uuid::new_v4()}
-                       , BookRecord{price:3995.0, size:0.5, id:Uuid::new_v4()}],
-                  vec![BookRecord{price:4005.0, size:0.4, id:Uuid::new_v4()}
-                     , BookRecord{price: 4005.02, size: 0.2, id: Uuid::new_v4()}]);
+        ob.reload(
+            vec![
+                BookRecord {
+                    price: 3994.96,
+                    size: 0.3,
+                    id: Uuid::new_v4(),
+                },
+                BookRecord {
+                    price: 3995.0,
+                    size: 0.5,
+                    id: Uuid::new_v4(),
+                },
+            ],
+            vec![
+                BookRecord {
+                    price: 4005.0,
+                    size: 0.4,
+                    id: Uuid::new_v4(),
+                },
+                BookRecord {
+                    price: 4005.02,
+                    size: 0.2,
+                    id: Uuid::new_v4(),
+                },
+            ],
+        );
 
-        ob.open(Side::Buy, BookRecord{price:3994.96, size:0.2, id: Uuid::new_v4()});
+        ob.open(
+            Side::Buy,
+            BookRecord {
+                price: 3994.96,
+                size: 0.2,
+                id: Uuid::new_v4(),
+            },
+        );
 
         let str = format!("{}", ob);
         assert_eq!(str, "OB: 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.5,0,0,0,0.5 | 3995.00   4005.00 | 0.4,0,0.2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
@@ -161,12 +189,41 @@ mod tests {
         let mut ob = OrderBook::new();
         let id1 = Uuid::new_v4();
         let id2 = Uuid::new_v4();
-        ob.reload(vec![BookRecord{price:3994.96, size:0.3, id:id1}
-                       , BookRecord{price:3995.0, size:0.5, id:id2}],
-                  vec![BookRecord{price:4005.0, size:0.4, id:Uuid::new_v4()}
-                     , BookRecord{price: 4005.02, size: 0.2, id: Uuid::new_v4()}]);
+        ob.reload(
+            vec![
+                BookRecord {
+                    price: 3994.96,
+                    size: 0.3,
+                    id: id1,
+                },
+                BookRecord {
+                    price: 3995.0,
+                    size: 0.5,
+                    id: id2,
+                },
+            ],
+            vec![
+                BookRecord {
+                    price: 4005.0,
+                    size: 0.4,
+                    id: Uuid::new_v4(),
+                },
+                BookRecord {
+                    price: 4005.02,
+                    size: 0.2,
+                    id: Uuid::new_v4(),
+                },
+            ],
+        );
 
-        ob.open(Side::Buy, BookRecord{price:3994.96, size:0.2, id:Uuid::new_v4()});
+        ob.open(
+            Side::Buy,
+            BookRecord {
+                price: 3994.96,
+                size: 0.2,
+                id: Uuid::new_v4(),
+            },
+        );
         ob._match(3995.0, 0.5, id2);
 
         let str = format!("{}", ob);
@@ -178,10 +235,32 @@ mod tests {
         let mut ob = OrderBook::new();
         let id1 = Uuid::new_v4();
         let id2 = Uuid::new_v4();
-        ob.reload(vec![BookRecord{price:3994.96, size:0.3, id:id1}
-                       , BookRecord{price:3995.0, size:0.5, id:id2}],
-                  vec![BookRecord{price:4005.0, size:0.4, id:Uuid::new_v4()}
-                     , BookRecord{price: 4005.02, size: 0.2, id: Uuid::new_v4()}]);
+        ob.reload(
+            vec![
+                BookRecord {
+                    price: 3994.96,
+                    size: 0.3,
+                    id: id1,
+                },
+                BookRecord {
+                    price: 3995.0,
+                    size: 0.5,
+                    id: id2,
+                },
+            ],
+            vec![
+                BookRecord {
+                    price: 4005.0,
+                    size: 0.4,
+                    id: Uuid::new_v4(),
+                },
+                BookRecord {
+                    price: 4005.02,
+                    size: 0.2,
+                    id: Uuid::new_v4(),
+                },
+            ],
+        );
 
         ob.done(3994.96, id1);
 
